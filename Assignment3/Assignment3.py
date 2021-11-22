86a6b8534b1081d88894689612b4f144f22bbc5b
HimaMadhu <94009528+HimaMadhu@users.noreply.github.com>
2021-11-22 23:50:50 +0530

    Update Assignment3.py

--- Assignment3/Assignment3.py
+++ Assignment3/Assignment3.py
@@ -1,1 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sympy import *
 
+a, b, c = symbols('a b c')
+
+A = np.array([a,b+c])
+B = np.array([a,b-c])
+C = np.array([-a,c])
+D = np.vstack((A-B,A-C))
+Area = (D[0][0]*D[1][1]-D[0][1]*D[1][0])/2
+
+print("Area of the trainagle formed by (a,b+c), (a,b-c) and (-a,c) is ",Area)
