813d3b9aef55f9c27f8ba8b8f7095263b781921d
HimaMadhu <94009528+HimaMadhu@users.noreply.github.com>
2021-11-24 16:01:46 +0530

    Update Assignment3.py

--- Assignment3/Assignment3.py
+++ Assignment3/Assignment3.py
@@ -8,6 +8,6 @@ A = np.array([a,b+c])
 B = np.array([a,b-c])
 C = np.array([-a,c])
 D = np.vstack((A-B,A-C))
-Area = (D[0][0]*D[1][1]-D[0][1]*D[1][0])/2
+Area = abs((D[0][0]*D[1][1]-D[0][1]*D[1][0])/2)
 
 print("Area of the trainagle formed by (a,b+c), (a,b-c) and (-a,c) is ",Area)
